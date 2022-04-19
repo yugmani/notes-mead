2e7a25b38315dd5862b8885a882e20efd0555f49
Himalaya <yugmani@yahoo.com>
2022-04-19 01:06:42 -0700

    yargs command:add, remove, read, list

--- server.js
+++ server.js
@@ -1,17 +1,127 @@
 const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-let myEmail = "ymg@yahoo.com";
+//validating and log colorful output
+// ***************************************
 
-let isValid = validator.isEmail(myEmail);
-let isEmpty = validator.isEmpty(myEmail);
-let url = validator.isURL(
-  "https://www.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728848#questions"
-);
+// let myEmail = "ymg@yahoo.com";
 
-console.log(chalk.blue(isValid), chalk.white.bold(myEmail));
-console.log(chalk.yellow.strikethrough(isEmpty), chalk.cyan.underline(myEmail));
-console.log(chalk.red.italic(url), chalk.bgYellow(myEmail));
+// let isValid = validator.isEmail(myEmail);
+// let isEmpty = validator.isEmpty(myEmail);
+// let url = validator.isURL(
+//   "https://www.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728848#questions"
+// );
+
+// console.log(chalk.blue(isValid), chalk.white.bold(myEmail));
+// console.log(chalk.yellow.strikethrough(isEmpty), chalk.cyan.underline(myEmail));
+// console.log(chalk.red.italic(url), chalk.bgYellow(myEmail));
 
 //Disclaimer: the latest one does not support
 //$ npm i chalk@2.4.1
+
+// process.argv
+// **************************
+
+console.log(process.argv);
+//   'C:\\Program Files\\nodejs\\node.exe',
+// 'C:\\Users\\yugma\\js-node\\notes-mead\\server.js',
+// 'yoog'
+// console.log(process.argv[0]); //C:\Program Files\nodejs\node.exe
+// console.log(process.argv[1]); //C:\Users\yugma\js-node\notes-mead\server.js
+// console.log(process.argv[2]); //yoog
+
+//conditional arguments and options
+// ****************************
+
+// const command = process.argv[2];
+
+// if (command === "add") {
+//   console.log("Adding note!");
+// } else if (command === "remove") {
+//   console.log("Removing note!");
+// } else {
+//   console.log("Choose either 'add' or 'remove'");
+// }
+
+//using yargs
+// **************************
+
+// console.log(yargs.argv);
+// { _: [], '$0': 'server.js' }
+
+// node server.js add --title="Things to buy";
+//{ _: [ 'add' ], title: 'Things to buy', '$0': 'server.js' }
+
+//noder server.js --help
+// Options:
+// --help     Show help                                              [boolean]  --version  Show version number
+
+//noder server.js --version
+// 1.0.0
+
+//customise yargs version
+yargs.version("1.1.0");
+//noder server.js --version
+//1.1.0
+
+//add, remove, read, list with yargs
+// ************************************
+
+//create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note");
+  },
+});
+
+//node server.js add
+//Adding a new note
+// { _: [ 'add' ], '$0': 'server.js' }
+
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
+
+//node server.js remove
+// Removing the note
+// { _: [ 'remove' ], '$0': 'server.js' }
+
+//node server.js --help
+// Commands:
+// server.js add     Add a new note
+// server.js remove  Remove a note
+
+// create list command
+yargs.command({
+  command: "list",
+  describe: "Listing a note",
+  handler: function () {
+    console.log("Listing all my notes");
+  },
+});
+
+//node server.js list
+// Listing all my notes
+// { _: [ 'list' ], '$0': 'server.js' }
+
+yargs.command({
+  command: "read",
+  describe: "Reading a note",
+  handler: function () {
+    console.log("Reading my note!");
+  },
+});
+
+//node server.js read
+// Reading my note!
+// { _: [ 'read' ], '$0': 'server.js' }
+
+console.log(yargs.argv);
